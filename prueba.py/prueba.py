6e58664e3a1bb7c7664dce843b954d850735025b
AlvaroRR7 <alvaro.rr7.14@gmail.com>
2026-04-14 16:34:39 -0400

    primer commit

---
+++ prueba.py/prueba.py
@@ -0,0 +1,27 @@
+# print("ingrese su nombre")
+# nombre=input()
+# edad=input(input("ingrese su edad") )
+
+# print("hola", nombre, "su edad es", edad)
+
+# if edad>=18:
+#     PRINT("ES MAYOR DE EDAD")
+# else:
+#     print("es mayor de edad")
+
+
+# validar si un numero es positivo
+
+
+num=int(imput("ingrese un numero: "))
+
+if num>0:
+    print("el numero es positivo")
+else: 
+    print ("el numero es negativo")
+print("elpromedio es", round(prom,1))
+
+if prom>=4:
+    print("el alumno aprobo")
+else:
+    print ("el alumno reprobo")
