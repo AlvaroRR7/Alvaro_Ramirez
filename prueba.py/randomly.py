6fd8aa7bddb0a174e57ae15fd5db539964bbd9fb
alvarorr7 <alvaro.rr7.14@gmail.com>
2026-04-28 17:02:16 -0400

    ejercicios

---
+++ prueba.py/randomly.py
@@ -0,0 +1,57 @@
+# uso y explicacion de random
+import random
+import time
+# num=random.randint(1, 10)
+# print(num)
+
+# for i in range(num):
+#     print("hola vicente")
+
+#     for i in range(10):
+#         print(f"{num}x{i}={num+i}")
+
+# ball=random.randint(60, 190)
+# ball1=random.randint(60, 190)
+# ball2=random.randint(60, 190)
+
+# j1=input("ingrese el nombre del jugador 1")
+# j2=input("ingrese el nombre del jugador 2")
+# j3=input("ingrese el nombre del jugador 3")
+# print(j1,"golpe la pelota a ",ball," metros!")
+# print(j2,"golpe la pelota a ",ball1," metros!")
+# print(j3,"golpe la pelota a ",ball2," metros!")
+
+# if j1>j2 and j1>j3:
+#     print(f"el jugador 1 lanzo la pelota mas lejos")
+# elif j2>j1 and j3>j2:
+#     print (f"el jugador 2 lanzo la pelota mas lejos")
+# elif j3>j1 and j3>j2:
+#     print(f"el jugador 3 lanzo la pelota mas lejos")
+
+# dado=random.randint(1,6)
+# dado1=random.randint(1,6)
+# print(f"el dado dio {dado} y el segundo dado dio {dado1}")
+# if dado==dado1:
+#     print ("usted se va a la carcel")
+# else:
+#     print("prosiga")
+
+# posicion=0
+# meta=50
+# turnos=1
+
+# while posicion<meta:
+#     dado1=random.randint(1,6)
+#     dado2=random.randint(1,6)
+
+#     print(f"el dado dio {dado1}")
+#     print(f"el dado dio {dado2}")
+#     posicion+=dado1+dado2
+#     time.sleep(1)
+#     print(f"el jugador esta en la posicion {posicion}")
+#     turnos+=1
+# print(f"ha llegado a la meta en {turnos} turno ")
+
+  
+
+
